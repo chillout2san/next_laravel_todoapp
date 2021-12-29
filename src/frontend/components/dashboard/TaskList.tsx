dc096d7f748bb646156607f671d7d328f868fc97
chillout2san <75463077+chillout2san@users.noreply.github.com>
2021-12-30 03:47:16 +0900

    feat: 詳細ボタン処理実装

--- src/frontend/components/dashboard/TaskList.tsx
+++ src/frontend/components/dashboard/TaskList.tsx
@@ -13,9 +13,17 @@ import {
   Th,
   Button,
   Td,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
 } from '@chakra-ui/react'
+import { useDisclosure } from '@chakra-ui/hooks'
 import { ALL, DONE, REQUESTED, WORK_ON_PROGRESS } from '../../constants'
 import { TodoType } from '../../types'
+import { useState } from 'react'
 
 interface PropType {
   todos: TodoType[]
@@ -23,6 +31,13 @@ interface PropType {
 
 const TaskList = (props: PropType) => {
   const { todos } = props
+  const [todo, setTodo] = useState<TodoType>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const openModal = (index: number) => {
+    const selectedTodo = todos[index]
+    setTodo(selectedTodo)
+    onOpen()
+  }
   return (
     <Box p={2} borderRadius="md" borderWidth="1px" shadow="lg">
       <Text pb={2} fontWeight="bold">
@@ -48,21 +63,25 @@ const TaskList = (props: PropType) => {
             <Th width="20%">ID</Th>
             <Th width="20%">名前</Th>
             <Th width="20%">状況</Th>
-            <Th width="20%"></Th>
-            <Th width="20%"></Th>
+            <Th width="20%">締切</Th>
+            <Th width="10%"></Th>
+            <Th width="10%"></Th>
           </Tr>
         </Thead>
         <Tbody>
           {todos &&
-            todos.map(({ title, content, status, deadline }, index) => {
+            todos.map(({ title, status, deadline }, index) => {
               return (
                 <Tr key={index}>
                   <Td>{index}</Td>
                   <Td>{title}</Td>
                   <Td>{status}</Td>
+                  <Td>{deadline}</Td>
                   <Td>
                     <Button bg="teal.300" size="xs">
-                      <Text color="white">詳細</Text>
+                      <Text color="white" onClick={() => openModal(index)}>
+                        詳細
+                      </Text>
                     </Button>
                   </Td>
                   <Td>
@@ -75,6 +94,18 @@ const TaskList = (props: PropType) => {
             })}
         </Tbody>
       </Table>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader bg="teal.300">
+              <Text color="white">「{todo && todo.title}」の詳細</Text>
+            </ModalHeader>
+            <ModalCloseButton />
+
+            <ModalBody bg="teal.50">{todo && todo.content}</ModalBody>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
     </Box>
   )
 }
